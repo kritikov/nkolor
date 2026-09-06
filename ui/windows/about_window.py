f7c293925010c63db99cee7699e1cba0f7145fb2
kritikov <kritikov@yahoo.com>
2026-09-06 08:54:18 +0300

    working

--- ui/windows/about_window.py
+++ ui/windows/about_window.py
@@ -1,6 +1,6 @@
 import gi
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk, Gdk, Gio
+from gi.repository import Gtk
 
 class AboutWindow(Gtk.ApplicationWindow):
 
@@ -21,7 +21,7 @@ class AboutWindow(Gtk.ApplicationWindow):
         app_name.add_css_class("about-title")
         root_child.append(app_name)
 
-        version = Gtk.Label(label="Version: 1.0.0")
+        version = Gtk.Label(label="Version: 1.1.0")
         version.add_css_class("about-subtitle")
         root_child.append(version)
 
--- ui/windows/main_window.py
+++ ui/windows/main_window.py
@@ -1,7 +1,7 @@
 import gi
 gi.require_version("Gtk", "4.0")
 
-from gi.repository import Gtk, Gdk, Gio
+from gi.repository import Gtk, Gdk
 from nkolor.ui.widgets.history_bar import HistoryBar
 from nkolor.ui.widgets.pick_button import PickButton
 from nkolor.ui.widgets.color_preview import ColorPreview
